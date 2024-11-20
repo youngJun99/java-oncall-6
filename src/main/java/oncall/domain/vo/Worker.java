6276a056b2ad75c31acda50113623d74969c30e4
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-21 01:40:44 +0900

    feat(Worker): 정적 펙토리 매서드 구현 및 equals 오버라이딩

--- src/main/java/oncall/domain/vo/Worker.java
+++ src/main/java/oncall/domain/vo/Worker.java
@@ -2,18 +2,37 @@ package oncall.domain.vo;
 
 import oncall.constants.Errors;
 
+import java.util.Objects;
+
 public class Worker {
 
     private final String name;
 
-    public Worker(String name) {
+    private Worker(String name) {
         validateName(name);
         this.name = name;
+    }
+
+    public static Worker from(String name){
+        return new Worker(name);
     }
 
     private void validateName(String inputName) {
         if (inputName.length() > 5) {
             throw new IllegalArgumentException(Errors.NAME_RANGE_ERROR.getMessage());
         }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Worker worker = (Worker) object;
+        return Objects.equals(name, worker.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
