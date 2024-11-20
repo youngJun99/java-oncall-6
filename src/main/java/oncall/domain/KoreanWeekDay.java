4fa3665dc3cfd7986f909402b4c9f4c2460a0e2b
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-20 18:41:56 +0900

    feat(KoreanWeekDay): 검증 로직과 get 로직 구현 완료

--- src/main/java/oncall/domain/KoreanWeekDay.java
+++ src/main/java/oncall/domain/KoreanWeekDay.java
@@ -1,29 +1,49 @@
 package oncall.domain;
 
-import java.time.DayOfWeek;
+import oncall.constants.Errors;
+
+import java.util.Arrays;
 
 public enum KoreanWeekDay {
-    월요일(DayOfWeek.of(1),"월"),
-    화요일(DayOfWeek.of(2),"화"),
-    수요일(DayOfWeek.of(3),"수"),
-    목요일(DayOfWeek.of(4),"목"),
-    금요일(DayOfWeek.of(5),"금"),
-    토요일(DayOfWeek.of(6),"토"),
-    일요일(DayOfWeek.of(7),"일");
+    월요일(1, "월"),
+    화요일(2, "화"),
+    수요일(3, "수"),
+    목요일(4, "목"),
+    금요일(5, "금"),
+    토요일(6, "토"),
+    일요일(7, "일");
 
-    private DayOfWeek dayOfWeek;
+    private int dayIndex;
     private String name;
 
-    KoreanWeekDay(DayOfWeek dayOfWeek,String name) {
-        this.dayOfWeek = dayOfWeek;
+    KoreanWeekDay(int dayIndex, String name) {
+        this.dayIndex = dayIndex;
         this.name = name;
     }
 
-    public DayOfWeek getDayOfWeek() {
-        return dayOfWeek;
+    public int getIndexOf(String input) {
+        validateWeekDay(input);
+        return Arrays.stream(KoreanWeekDay.values())
+                .filter(weekday -> weekday.name.equals(input))
+                .mapToInt(weekday -> weekday.dayIndex)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(Errors.SYSTEM_ERROR.getMessage()));
     }
 
-    public String getName() {
-        return name;
+    private void validateWeekDay(String input) {
+        boolean inValid = Arrays.stream(KoreanWeekDay.values())
+                .noneMatch(weekday -> weekday.name.equals(input));
+        if (inValid) {
+            throw new IllegalArgumentException(Errors.WEEK_DAY_NOT_VALID.getMessage());
+        }
     }
+
+    public String getNameOf(int index) {
+        return Arrays.stream(KoreanWeekDay.values())
+                .filter(weekday -> weekday.dayIndex == index)
+                .map(weekday -> weekday.name)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(Errors.SYSTEM_ERROR.getMessage()));
+    }
+
 }
