78d968c96a4dbb7fbef6409c0054cc0baa7cc7df
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-20 16:11:58 +0900

    refactor(MyMonth): 매서드 정렬

--- src/main/java/oncall/vo/MyMonth.java
+++ src/main/java/oncall/vo/MyMonth.java
@@ -11,14 +11,14 @@ public class MyMonth {
         this.month = month;
     }
 
+    public int getMonth() {
+        return month;
+    }
+
     private void validateMyMonth(int month) {
         if (month < 1 || month > 12) {
             throw new IllegalArgumentException(Errors.MONTH_NOT_VALID.getMessage());
         }
-    }
-
-    public int getMonth() {
-        return month;
     }
 
 }
