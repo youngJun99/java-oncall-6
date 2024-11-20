e1dfacc03e805f3f406a83549c456459784ef926
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-21 05:09:37 +0900

    feat(OutputView): 구현 완료

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,4 +1,29 @@
 package oncall.view;
 
+import oncall.dto.MatchedWorkDto;
+
+import java.util.List;
+
 public class OutputView {
+
+    private final static String REST_DAY_STRING = "(휴일) ";
+
+    public void printResult(List<MatchedWorkDto> workSchedule) {
+        workSchedule.forEach(day -> {
+            System.out.println(day.month() + "월 " +
+                    day.monthDay() + "일 " +
+                    day.koreanWeekDay() + " " +
+                    restDayFormat(day.restDay()) +
+                    day.worker()
+            );
+        });
+    }
+
+    private String restDayFormat(boolean restDay) {
+        if (restDay) {
+            return REST_DAY_STRING;
+        }
+        return "";
+    }
+
 }
