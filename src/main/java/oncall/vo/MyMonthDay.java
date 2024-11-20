79979a45f8866796fe8ef2a15c920ebd7fe8ec62
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-20 16:15:58 +0900

    feat(MyMonthDay): 클래스 구현 완료

---
+++ src/main/java/oncall/vo/MyMonthDay.java
@@ -0,0 +1,23 @@
+package oncall.vo;
+
+import oncall.constants.Errors;
+
+public class MyMonthDay {
+
+    private int monthDay;
+
+    public MyMonthDay(int monthDay) {
+        validateMonthDay(monthDay);
+        this.monthDay = monthDay;
+    }
+
+    public int getMonthDay() {
+        return monthDay;
+    }
+
+    private void validateMonthDay(int monthDay) {
+        if(monthDay < 1 || monthDay > 32) {
+            throw new IllegalArgumentException(Errors.MONTH_DAY_OUT_OF_RANGE.getMessage());
+        }
+    }
+}
