3fa659f9ff4547ccef84e126b25869b6cc3756e5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-20 19:22:13 +0900

    feat(InputValidator): 생성 완료

--- src/main/java/oncall/handler/InputValidator.java
+++ src/main/java/oncall/handler/InputValidator.java
@@ -1,5 +1,12 @@
 package oncall.handler;
 
+import java.util.regex.Matcher;
+
 public interface InputValidator {
 
+    void validateDateInfo(String string);
+
+    void validateWorkSchedule(String string);
+
+    Matcher getMatcher();
 }
