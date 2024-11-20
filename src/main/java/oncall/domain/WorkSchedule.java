5d5468eba81520f94c2525e8c1d3e6f360dedd8f
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-21 02:30:22 +0900

    feat(WorkSchedule): 클래스 로직 완성

--- src/main/java/oncall/domain/WorkSchedule.java
+++ src/main/java/oncall/domain/WorkSchedule.java
@@ -13,13 +13,58 @@ public class WorkSchedule {
     private static final int MAX_WORKERS = 35;
 
 
-    private final Queue<Worker> workSchedule = new LinkedList<>();
-    private final Queue<Worker> changedSchedule = new LinkedList<>();
+    private final Queue<Worker> workSchedule;
+    private final Queue<Worker> changedSchedule;
+    private int changed;
 
     public WorkSchedule(List<Worker> workers) {
         validateNumberOfWorkers(workers);
         validateDuplicateName(workers);
+        workSchedule = new LinkedList<>();
+        changedSchedule = new LinkedList<>();
+        changed = 0;
         workSchedule.addAll(workers);
+    }
+
+    public Worker getFirstWorker() {
+        Worker workerToReturn = workSchedule.poll();
+        workSchedule.add(workerToReturn);
+        return workerToReturn;
+    }
+
+    public Worker getNextWorker(Worker currentWorker) {
+        if (changedSchedule.isEmpty()) {
+            return ProcessNormalOrder(currentWorker);
+        }
+        return ProcessChangedOrder(currentWorker);
+    }
+
+    private Worker ProcessChangedOrder(Worker currentWorker) {
+        if (currentWorker.equals(changedSchedule.peek())) {
+            changed++;
+            Worker workerToReturn = workSchedule.poll();
+            changedSchedule.add(workerToReturn);
+            return workerToReturn;
+        }
+        Worker workerToReturn = changedSchedule.poll();
+        workSchedule.add(workerToReturn);
+        while (changed != 0) {
+            workSchedule.add(changedSchedule.poll());
+        }
+        return workerToReturn;
+    }
+
+    private Worker ProcessNormalOrder(Worker currentWorker) {
+        if (currentWorker.equals(workSchedule.peek())) {
+            changed++;
+            changedSchedule.add(workSchedule.poll());
+            Worker workerToReturn = workSchedule.poll();
+            changedSchedule.add(workerToReturn);
+            return workerToReturn;
+        }
+        Worker workerToReturn = workSchedule.poll();
+        workSchedule.add(workerToReturn);
+        return workerToReturn;
     }
 
     private void validateNumberOfWorkers(List<Worker> inputWorkers) {
