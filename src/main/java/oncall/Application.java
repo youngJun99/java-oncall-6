ec297d484a1df8da3876e47228258b885de04c40
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-21 05:16:41 +0900

    feat(Application): 구현 완료

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,11 @@
 package oncall;
 
+import oncall.controller.OnCallController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        OnCallController onCallController = appConfig.onCallController();
+        onCallController.run();
     }
 }
