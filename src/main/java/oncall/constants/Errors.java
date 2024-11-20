6214d2f653095f19bc918848292a3ab4e25f0d68
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-20 18:51:27 +0900

    chore(Errors): 주석 수정

--- src/main/java/oncall/constants/Errors.java
+++ src/main/java/oncall/constants/Errors.java
@@ -5,7 +5,7 @@ public enum Errors {
     //시스템 에러
     SYSTEM_ERROR("시스템 구조 에러"),
 
-    //날짜 설정 관련 에러
+    //날짜 입력관련 에러
     MONTH_OUT_OF_RANGE("월의 입력범위 오류"),
     MONTH_DAY_OUT_OF_RANGE("일자 입력 범위 오류"),
     DATE_NOT_VALID("존재할 수 없는 날짜입니다"),
