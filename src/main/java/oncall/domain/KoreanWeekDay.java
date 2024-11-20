c0a726d656f0b3ae0f6661104d5e57c8e36433d5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-20 15:42:25 +0900

    feat(KoreanWeekDay): 상수 생성완료

---
+++ src/main/java/oncall/domain/KoreanWeekDay.java
@@ -0,0 +1,21 @@
+package oncall.domain;
+
+import java.time.DayOfWeek;
+
+public enum KoreanWeekDay {
+    월요일(DayOfWeek.of(1),"월"),
+    화요일(DayOfWeek.of(2),"화"),
+    수요일(DayOfWeek.of(3),"수"),
+    목요일(DayOfWeek.of(4),"목"),
+    금요일(DayOfWeek.of(5),"금"),
+    토요일(DayOfWeek.of(6),"토"),
+    일요일(DayOfWeek.of(7),"일");
+
+    private DayOfWeek dayOfWeek;
+    private String name;
+
+    KoreanWeekDay(DayOfWeek dayOfWeek,String name) {
+        this.dayOfWeek = dayOfWeek;
+        this.name = name;
+    }
+}
