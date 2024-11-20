925a8dfa6d559a19d6c977cbcaeeb4c3ac274743
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-21 04:31:35 +0900

    feat(DateCalenderService): 서비스 구현 완료

--- src/main/java/oncall/service/DateCalenderService.java
+++ src/main/java/oncall/service/DateCalenderService.java
@@ -1,6 +1,36 @@
 package oncall.service;
 
+import oncall.domain.Holiday;
+import oncall.domain.KoreanWeekDay;
+import oncall.domain.MyDate;
+import oncall.domain.vo.MyMonth;
+import oncall.dto.StartDateDto;
+import oncall.dto.WorkDateDto;
+import oncall.handler.InputHandler;
+
+import java.util.List;
+
+import static oncall.domain.KoreanWeekDay.getIndexOf;
+import static oncall.utils.ErrorCatcher.retryHandler;
+
 public class DateCalenderService {
 
+    private final InputHandler inputHandler;
+    private MyDate myDate;
 
+    public DateCalenderService(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
+    public List<WorkDateDto> getWorkDateCalender() {
+        myDate = retryHandler(this::InitializeMyDate);
+        return myDate.getWorkDateCalender();
+    }
+
+    private MyDate InitializeMyDate() {
+        StartDateDto startDate = inputHandler.getDateInfo();
+        MyMonth startMonth = new MyMonth(startDate.month());
+        int startWeekDayIndex = getIndexOf(startDate.koreanWeekDay());
+        return new MyDate(startMonth,startWeekDayIndex);
+    }
 }
