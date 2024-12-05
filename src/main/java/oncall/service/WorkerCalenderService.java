c7164f7c8d91986114dba92e427ff07aec07d689
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-05 21:43:26 +0900

    feat: WorkerCalenderService 구현 완료

---
+++ src/main/java/oncall/service/WorkerCalenderService.java
@@ -0,0 +1,74 @@
+package oncall.service;
+
+import oncall.domain.Date;
+import oncall.domain.Worker;
+import oncall.domain.Workers;
+import oncall.dto.WorkSchedule;
+import oncall.handler.InputHandler;
+import oncall.utils.ErrorCatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static oncall.constants.Holiday.isHoliday;
+
+public class WorkerCalenderService {
+
+    private final InputHandler inputHandler;
+    private Date date;
+    private Workers weekdayWorkers;
+    private Workers weekEndWorkers;
+
+
+    public WorkerCalenderService(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
+    public List<WorkSchedule> generateWorkCalender() {
+        this.date = ErrorCatcher.returnRetryHandler(inputHandler::getDateFromUser);
+        ErrorCatcher.voidRetryHandler(this::initializeWorkers);
+
+        List<WorkSchedule> workCalender = new ArrayList<>();
+        Worker workedWorker = getFirstWorker();
+        workCalender.add(generateWorkSchedule(workedWorker));
+
+        while (date.hasNextDay()) {
+            workedWorker = getNextWorker(workedWorker);
+            workCalender.add(generateWorkSchedule(workedWorker));
+        }
+
+        return workCalender;
+    }
+
+    private Worker getFirstWorker() {
+        if (!date.isWorkDay() || isHoliday(date)) {
+            return weekdayWorkers.getFirstWorker();
+        }
+        return weekEndWorkers.getFirstWorker();
+    }
+
+    private Worker getNextWorker(Worker previousWorker) {
+        if (!date.isWorkDay() || isHoliday(date)) {
+            return weekdayWorkers.getNextWorker(previousWorker);
+        }
+        return weekEndWorkers.getNextWorker(previousWorker);
+    }
+
+    private WorkSchedule generateWorkSchedule(Worker worker) {
+        WorkSchedule workSchedule = new WorkSchedule(
+                date.getMonth(),
+                date.getDay(),
+                date.getDayOfWeek(),
+                worker.getName(),
+                isHoliday(date) && date.isWorkDay());
+        date.nextDay();
+        return workSchedule;
+    }
+
+
+    private void initializeWorkers() {
+        this.weekdayWorkers = inputHandler.getWeekDayWorkers();
+        this.weekEndWorkers = inputHandler.getWeekEndWorkers();
+    }
+
+}
