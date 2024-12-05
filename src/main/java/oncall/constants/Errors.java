98d7606b9e63169e3575a603b0636bb7f09059c0
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-05 19:29:06 +0900

    feat: 순번에 중복되는 사원들에 대한 에러 추가

--- src/main/java/oncall/constants/Errors.java
+++ src/main/java/oncall/constants/Errors.java
@@ -4,7 +4,8 @@ public enum Errors {
 
     //도메인 관련 에러
     WORKER_NAME_RANGE("사원의 이름은 %d 글자 이하여야 합니다."),
-    WORKERS_RANGE("사원은 최소 %d명 이상 최대 %d명 이하여야 합니다.");
+    WORKERS_RANGE("순번을 구성하는 사원은 최소 %d명 이상 최대 %d명 이하여야 합니다."),
+    DUPLICATE_WORKERS("순번에 중복된 사원이 존재합니다.");
 
 
     private static final String PREFIX = "[ERROR] ";
@@ -16,6 +17,6 @@ public enum Errors {
     }
 
     public String getMessage() {
-        return PREFIX+message;
+        return PREFIX + message;
     }
 }
