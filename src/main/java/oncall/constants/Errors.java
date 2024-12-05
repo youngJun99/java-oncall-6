3c10290db0ba2a4cdb7d16bd1f75845b8a913709
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-05 20:19:48 +0900

    feat: 입력 형태 관련한 에러 추가

--- src/main/java/oncall/constants/Errors.java
+++ src/main/java/oncall/constants/Errors.java
@@ -6,11 +6,12 @@ public enum Errors {
     //사원 관련
     WORKER_NAME_RANGE("사원의 이름은 %d 글자 이하여야 합니다."),
     WORKERS_RANGE("순번을 구성하는 사원은 최소 %d명 이상 최대 %d명 이하여야 합니다."),
+    INVALID_WORKER_INPUT("사원들의 입력 형식이 올바르지 않습니다."),
     DUPLICATE_WORKERS("순번에 중복된 사원이 존재합니다."),
 
     //날짜 관련
     MONTH_RANGE("월은 1월 부터 12월 사이를 입력해야 합니다."),
-    DAY_OF_WEEK("요일의 입력 형식이 올바르지 않습니다..");
+    DATE_INPUT("요일의 입력 형식이 올바르지 않습니다..");
 
 
     private static final String PREFIX = "[ERROR] ";
