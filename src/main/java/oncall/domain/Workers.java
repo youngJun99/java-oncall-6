7fdf9c5525821359bb98d6da47650a689ed34783
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-05 19:33:31 +0900

    feat: Workers 구현 및 validation 구현

--- src/main/java/oncall/domain/Workers.java
+++ src/main/java/oncall/domain/Workers.java
@@ -1,4 +1,36 @@
 package oncall.domain;
 
+import oncall.constants.Errors;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Workers {
+
+    private static final int WORKER_LENGTH_UPPER_LIMIT = 35;
+    private static final int WORKER_LENGTH_LOWER_LIMIT = 5;
+
+    private final LinkedList<Worker> workers;
+
+    public Workers(List<String> workers) {
+        validateDuplicate(workers);
+        validateLength(workers);
+        this.workers = workers.stream()
+                .map(Worker::new)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    private void validateDuplicate(List<String> workers) {
+        if (workers.size() != workers.stream().distinct().count()) {
+            throw new IllegalArgumentException(Errors.DUPLICATE_WORKERS.getMessage());
+        }
+    }
+
+    private void validateLength(List<String> workers) {
+        if (workers.size() < WORKER_LENGTH_LOWER_LIMIT || workers.size() > WORKER_LENGTH_UPPER_LIMIT) {
+            throw new IllegalArgumentException(String.format(Errors.WORKERS_RANGE.getMessage(), WORKER_LENGTH_LOWER_LIMIT, WORKER_LENGTH_UPPER_LIMIT));
+        }
+    }
+
 }
