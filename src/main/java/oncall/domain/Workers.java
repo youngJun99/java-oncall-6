e843026184c4a7ca1b61080865ed8932cbce2a7b
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-12 17:53:00 +0900

    feat: Workers 구현 및 리드미 업데이트

---
+++ src/main/java/oncall/domain/Workers.java
@@ -0,0 +1,38 @@
+package oncall.domain;
+
+import oncall.constants.Errors;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Workers {
+
+    private static final int WORKERS_RANGE_UPPER_LIMIT = 35;
+    private static final int WORKERS_RANGE_LOWER_LIMIT = 5;
+
+    private final List<Worker> workers;
+
+    public Workers(List<String> workers) {
+        validateLength(workers);
+        validateDuplicate(workers);
+        this.workers = workers.stream()
+                .map(Worker::new)
+                .collect(Collectors.toList());
+    }
+
+    private void validateLength(List<String> workers) {
+        if(workers.size() < WORKERS_RANGE_LOWER_LIMIT || workers.size() > WORKERS_RANGE_UPPER_LIMIT){
+            throw new IllegalArgumentException(String.format(Errors.WORKERS_LENGTH.getMessage(),WORKERS_RANGE_LOWER_LIMIT,WORKERS_RANGE_UPPER_LIMIT));
+        }
+    }
+
+    private void validateDuplicate(List<String> workers) {
+        if(workers.size() != workers.stream()
+                .distinct()
+                .count()) {
+            throw new IllegalArgumentException(Errors.DUPLICATE_WORKERS.getMessage());
+        }
+    }
+}
+
+
