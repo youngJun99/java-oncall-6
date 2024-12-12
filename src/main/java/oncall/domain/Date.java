da9578d944e94dbbcdc87fe276437086b7d0a611
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-12 18:24:11 +0900

    feat: Date 구현 및 리드미 업데이트

--- src/main/java/oncall/domain/Date.java
+++ src/main/java/oncall/domain/Date.java
@@ -1,11 +1,60 @@
 package oncall.domain;
 
+import oncall.constants.DayOfWeek;
+import oncall.constants.Errors;
+import oncall.constants.Holiday;
+
+import java.util.List;
+
 public class Date {
+
+    private static final int DECEMBER = 12;
+    private static final int JANUARY = 1;
+    private static final int START_OF_MONTH = 1;
 
     private final int month;
     private int day;
-    private final
+    private final int lastDayOfMonth;
+    private DayOfWeek dayOfWeek;
+
+    public Date(int month, DayOfWeek dayOfWeek) {
+        validateMonthRange(month);
+        this.month = month;
+        this.day = START_OF_MONTH;
+        this.lastDayOfMonth = getLastDayOfMonth(month);
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public boolean isRestDay() {
+        return DayOfWeek.isWeekEnd(dayOfWeek) || Holiday.isHoliday(month,day);
+    }
+
+    public void toNextDay() {
+        day++;
+        this.dayOfWeek = DayOfWeek.getNextDayOfWeek(dayOfWeek);
+    }
+
+    public boolean needNextEmergencyWorker() {
+        return day <= lastDayOfMonth;
+    }
 
 
+    private void validateMonthRange(int month) {
+        if(month < JANUARY || month > DECEMBER) {
+            throw new IllegalArgumentException(String.format(Errors.MONTH_RANGE.getMessage(),JANUARY,DECEMBER));
+        }
+    }
+
+    private int getLastDayOfMonth(int month) {
+        if(month == 2) {
+            return 28;
+        }
+        if(List.of(4,5,9,11).contains(month)){
+            return 30;
+        }
+        return 31;
+    }
 }
+
+
 
