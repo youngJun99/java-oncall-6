ce4999fc3a9089fd30dc6d3f9c7ab895556966d8
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-12 17:39:50 +0900

    feat: 근무자 이름길이 관련 에러를 추가

--- src/main/java/oncall/constants/Errors.java
+++ src/main/java/oncall/constants/Errors.java
@@ -3,7 +3,10 @@ package oncall.constants;
 public enum Errors {
 
     //재시도 횟수 관련 에러
-    OVER_MAX_RETRIES("최대 재시도 횟수 %d번을 초과했습니다.");
+    OVER_MAX_RETRIES("최대 재시도 횟수 %d번을 초과했습니다."),
+
+    //직원 관련 에러
+    WORKER_NAME_LENGTH("직원의 이름은 %d글자를 넘어가면 안됩니다.");
 
 
     private static final String PREFIX = "[ERROR] ";
