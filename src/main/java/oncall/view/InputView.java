309fa7e2aed28e2c0e0af202f9edfac409ceadc2
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-12 18:26:19 +0900

    feat: InputView 구현완료

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,4 +1,24 @@
 package oncall.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+
+    public String getMonthAndDayOfWeek() {
+        System.out.println("비상 근무를 배정할 월과 시작 요일을 입력하세요>");
+        return readLine();
+    }
+
+    public String getWeekDayWorkers() {
+        System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+        return readLine();
+    }
+
+    public String getWeekEndWorkers() {
+        System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+        return readLine();
+    }
 }
+
+
+
