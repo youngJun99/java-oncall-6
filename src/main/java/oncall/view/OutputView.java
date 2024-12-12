35b0d588964040dfb3087b85edc79cfc028f89ca
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-12 21:24:23 +0900

    feat: OutputView 구현 완료

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,4 +1,31 @@
 package oncall.view;
 
+import oncall.dto.WorkScheduleDto;
+
+import java.util.List;
+
 public class OutputView {
+
+    private static final String printFormat = "%d월 %d일 %s%s %s";
+    private static final String NONE_WEEK_END_REST_DAY = "휴일";
+
+    public void printSchedule(List<WorkScheduleDto> workScheduleDtos) {
+        workScheduleDtos.forEach(workSchedule-> {
+            if(workSchedule.notWeekEndRestDay()) {
+                printSchedule(workSchedule, NONE_WEEK_END_REST_DAY);
+                return;
+            }
+            printSchedule(workSchedule, "");
+        });
+
+    }
+
+    private static void printSchedule(WorkScheduleDto workSchedule, String x) {
+        System.out.println(String.format(printFormat,
+                workSchedule.month(),
+                workSchedule.day(),
+                workSchedule.dayOfWeek().name(),
+                x,
+                workSchedule.worker().getName()));
+    }
 }
